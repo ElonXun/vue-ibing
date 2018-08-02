44aa55597373b4219adec2e68dd38a2c80cf7cfb
fmn <cdnxyf@gmail.com>
2018-08-02 23:27:35 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    HelloWorld
+    <button @click="getOauth">申请鉴权</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+  methods: {
+    getOauth: function(){
+      // axios.get('http://localhost:3000/getOauth')
+      //   .then(function (response) {
+      //     console.log(response);
+      //   })
+      //   .catch(function (error) {
+      //     console.log(error);
+      //   });
+      console.log("getOauth")
+      window.open('http://localhost:3000/getOauth', '_self')
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
